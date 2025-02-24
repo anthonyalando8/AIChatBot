a305965f3b0fc531f099dd981ef37a57e20e7a5f
Daltone123 <daltone123@gmail.com>
2025-02-24 21:37:45 +0300

    about

--- chat/urls.py
+++ chat/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('signup/', views.signup_view, name='signup'),
     path('login/', views.login_view, name='login'),
     path('about/', views.about_view, name='about'),
+    
 ]
